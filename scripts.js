0779be4593032fcf25f51394d1bb286faa756864
Shawn Quinn <Shawn95GT@gmail.com>
2024-02-08 22:02:25 -0700

    WIP sync

---
+++ scripts.js
@@ -0,0 +1,40 @@
+
+/*
+Coding Steps:
+Create a menu app as seen in this week’s video. What you create is up to you as long as it meets the following requirements:
+Use at least one array.
+Use at least two classes.
+Your menu should have the options to create, view, and delete elements.
+
+let modelArray [];
+
+*/
+
+
+
+class Craft {
+    constructor(name, type) {
+        this.name=name
+        this.type=type
+    }
+    
+    describe() {
+        return `${this.name} is a ${this.type} type UAS`
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
