4ad7fdd120757718ebe7ed62aba7f1be0f06085e
Shawn Quinn <Shawn95GT@gmail.com>
2024-02-09 08:38:46 -0700

    WIP sync

--- scripts.js
+++ scripts.js
@@ -30,11 +30,73 @@ class Fleet {
         this.crafts = []
     }
 
-    addCraft(craft)
+    addCraft(craft) {
+        if (craft instanceof Craft) {
+            this.crafts.push(craft);
+        } else {
+            throw new Error (`You can only add an instance of Craft.  Argument is not a Craft: ${craft}`);
+        }
+    }
+
+    describe() {
+        return `${this.name} has ${this.crafts.length} crafts` ;
+    }
+}
+
+class Menu {
+    constructor() {
+        this.fleets = [];
+        this.selectedFleet = null;
+    }
+
+    start() {
+        let selection = this.showMainMenuOptions();
+        while (selection != 0) {
+            switch (selection) {
+                case '1':
+                    this.createFleet();
+                    break;
+                case '2':
+                    this.viewFleet();
+                    break
+                case '3':
+                    this.deleteFleet();
+                    break
+                case '4':
+                    this.displayFleets();
+                    break;
+                default:
+                    selection = 0;
+            }
+            selection = this.showMainMenuOptions();
+        }
+        alert('Goodbye!')
+    }
+
+    showMainMenuOptions() {
+        return prompt(`
+        0) Exit
+        1) Create New Fleet
+        2) View Fleet
+        3) Delete Fleet
+        4) display All Fleets
+        `);
+    }
+
+    displayFleets() {
+        let fleetString = '';  /* quotes? */
+        for (let i = 0; i < this.teams.length; i++) {
+            fleetString += i + ') ' +this.fleets[i].name + '\n';
+        }
+        alert(fleetString);
+    }
+
+
 }
 
 
-/* 2:30 */
+
+/* 13:02 */
 
 
 
