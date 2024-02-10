214f31dc7dbb60a8254c3c45a9eccc63bc49eb94
Shawn Quinn <Shawn95GT@gmail.com>
2024-02-09 22:52:44 -0700

    WIP sync

--- scripts.js
+++ scripts.js
@@ -79,22 +79,76 @@ class Menu {
         1) Create New Fleet
         2) View Fleet
         3) Delete Fleet
-        4) display All Fleets
+        4) Display All Fleets
+        `);
+    }
+
+    showFleetMenuOptions(fleetInfo) {
+        return prompt(`
+            0) back
+            1) Create Craft
+            2) Delete Craft
+            ---------------------
+            ${fleetInfo}
         `);
     }
 
     displayFleets() {
-        let fleetString = '';  /* quotes? */
-        for (let i = 0; i < this.teams.length; i++) {
+        let fleetString = '';
+        for (let i = 0; i < this.fleets.length; i++) {
             fleetString += i + ') ' +this.fleets[i].name + '\n';
         }
         alert(fleetString);
     }
 
+    createFleet() {
+        let name = prompt('Enter name for the new Fleet');
+        this.fleets.push(new Fleet(name));
+    }
+
+    viewFleet() {
+        let index = prompt('Enter the Index of the Fleet you wish to view:');
+        if (index > -1 && index < this.fleets.length) {
+            this.selectedFleet = this.fleets[index];
+            let description = 'Fleet Name: ' + this.selectedFleet.name + '\n';
+            for(let i = 0; i < this.selectedFleet.crafts.length; i++) {
+                description += i + ') ' + this.selectedFleet.crafts[i].name + ' - ' + this.selectedFleet.crafts[i].type + '\n';
+            }
+            let selection = this.showFleetMenuOptions(description);
+            switch (selection) {
+                case '1':
+                    this.createCraft();
+                    break
+                case '2':
+                    this.deleteCraft();
+            }
+        }
+    }
+
+deleteFleet() {
+    let index = prompt('Enter the index of the Fleet you Wish to delete: ');
+    if (index > -1 && index < this.fleets.length) {
+        this.fleets.splice(index, 1);
+    }
+}
+
+createCraft() {
+    let name = prompt('Enter name for new Craft: ')
+    let type = prompt('Enter type of UAS: ')
+    this.selectedFleet.crafts.push(new Craft(name, type));
+}
+
+deleteCraft() {
+    let index = prompt('Enter the index of the Craft you Wish to delete: ');
+    if (index > -1 && index < this.selectedFleet.crafts.length) {
+        this.selectedFleet.crafts.splice(index, 1);
+    }
+}
 
 }
 
-
+let menu = new Menu();
+menu.start();
 
 /* 13:02 */
 
